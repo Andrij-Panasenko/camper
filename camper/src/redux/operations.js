6d6e7b37e9ff603e8efc20cb869d31b81afae1b1
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-19 22:54:28 +0200

    delete auth fn

--- camper/src/redux/operations.js
+++ camper/src/redux/operations.js
@@ -2,10 +2,3 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://65f9eea93909a9a65b19aacf.mockapi.io";
 
-const setAuthHeader = (token) => {
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
-
-const clearAuthHeader = () => {
-  axios.defaults.headers.common.Authorization = "";
-};
