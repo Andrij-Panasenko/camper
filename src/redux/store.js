5119068ab53c2ab8556c2f19b158367d205c38fa
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-19 23:19:22 +0200

    persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { catalogReducer } from "./catalogSlice";
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistReducer,
+} from "redux-persist";
 
 export const store = configureStore({
   reducer: {
