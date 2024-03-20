70b52df84c7eebd95d2895736b5f827351b42003
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-20 20:57:31 +0200

    add styles

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { store } from "./redux/store";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename="/camper">
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <BrowserRouter basename="/camper">
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </React.StrictMode>
+
+    <GlobalStyles />
+  </>
 );
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,4 +1,17 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://65f9eea93909a9a65b19aacf.mockapi.io";
 
+export const getAllCampers = createAsyncThunk(
+  "get/campers",
+  async (_, thunkAPI) => {
+    try {
+      const data = await axios.get("/adverts");
+      return data;
+    } catch (error) {
+      console.log(error.message);
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
