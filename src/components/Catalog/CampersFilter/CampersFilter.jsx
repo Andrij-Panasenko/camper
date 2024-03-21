0dd8ace2b8860a4c4c2c6a1ff7dbc0960e1a3ae5
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-21 22:05:46 +0200

    began makin filters

--- src/components/Catalog/CampersFilter/CampersFilter.jsx
+++ src/components/Catalog/CampersFilter/CampersFilter.jsx
@@ -1,30 +1,43 @@
+import { EquipButton, EquipmentList } from "./CampersFilter.styled";
+
 export const CampersFilter = () => {
   return (
     <>
       <div>
         <label>
+          Location
           <input type="text" />
         </label>
         <p>Filters</p>
         <div>
           <p>Vehicle equipment</p>
-          <ul>
-            <li>
-              <button type="button"></button>
-            </li>
-            <li>
-              <button type="button"></button>
-            </li>
-            <li>
-              <button type="button"></button>
-            </li>
-            <li>
-              <button type="button"></button>
-            </li>
-            <li>
-              <button type="button"></button>
-            </li>
-          </ul>
+          <EquipmentList>
+            <EquipButton>
+              <div>
+                <p>AC</p>
+              </div>
+            </EquipButton>
+            <EquipButton>
+              <div>
+                <p>Automatic</p>
+              </div>
+            </EquipButton>
+            <EquipButton>
+              <div>
+                <p>Kitchen</p>
+              </div>
+            </EquipButton>
+            <EquipButton>
+              <div>
+                <p>TV</p>
+              </div>
+            </EquipButton>
+            <EquipButton>
+              <div>
+                <p>Shower/WC</p>
+              </div>
+            </EquipButton>
+          </EquipmentList>
         </div>
         <div>
           <p>Vehicle type</p>
---
+++ src/components/Catalog/CampersFilter/CampersFilter.styled.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const EquipmentList = styled.ul`
+  width: 360px;
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 10px;
+  row-gap: 8px;
+`;
+
+export const EquipButton = styled.li`
+  border-radius: 10px;
+  padding: 17px 18px;
+  border: 1px solid rgba(16, 24, 40, 0.2);
+  cursor: pointer;
+
+  background-color: transparent;
+`;
