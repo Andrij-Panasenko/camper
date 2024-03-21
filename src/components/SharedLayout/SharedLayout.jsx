0eee3bc3e618e8f5446461c08bc87d247be5c565
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-21 20:53:02 +0200

    styles

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,18 +1,19 @@
 import { Suspense } from "react"
 import { Outlet } from "react-router-dom"
 import { GlobalStyles } from "../../styles/GlobalStyles";
+import { AppLayout } from "./SharedLayout.styled";
 
 export const SharedLayout = () => {
     return (
       <>
-        <div>
+        <AppLayout>
           <header>Тут буде хедер з навігацією</header>
           <main>
             <Suspense fallback={null}>
               <Outlet />
             </Suspense>
           </main>
-        </div>
+        </AppLayout>
 
         <GlobalStyles />
       </>
---
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const AppLayout = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  padding-bottom: 40px; //temporary
+`;
