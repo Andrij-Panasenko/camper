6ee0b7e0bd22cc0a01e01cbf7a0bdbf7a4b5f037
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-21 21:52:10 +0200

    summagy

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,11 +1,14 @@
 import { CatalogList } from '../Catalog/CatalogList/CatalogList'
 import {CampersFilter} from './CampersFilter/CampersFilter'
+import { CatalogWrapp } from './Catalog.styled'
 
 export const Catalog = () => {
     return (
         <>
-            <CampersFilter/>
-            <CatalogList/>
+           <CatalogWrapp>
+             <CampersFilter/>
+             <CatalogList/>
+           </CatalogWrapp>
         </>
     )
 }
---
+++ src/components/Catalog/Catalog.styled.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const CatalogWrapp = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 64px;
+`;
